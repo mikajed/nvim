dfc59be7e932e835a61261f9474a4f0ae944c16c
mikajed <michaeljedrz@gmail.com>
2023-11-17 15:55:14 +0100

    add neotree

--- init.lua
+++ init.lua
@@ -61,6 +61,16 @@ require("lazy").setup({
     opts={
         --Config goes here
     },
+},
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
 }
 })
 
